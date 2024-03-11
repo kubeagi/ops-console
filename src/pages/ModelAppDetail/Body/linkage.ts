246c608b4e7879c1cc64bb8fe2b2894826abfbcc
Jandiasnow <88074479@qq.com>
2024-03-11 10:04:17 +0800

    feat: change sdk version

--- src/pages/ModelAppDetail/Body/linkage.ts
+++ src/pages/ModelAppDetail/Body/linkage.ts
@@ -1,7 +1,7 @@
 export const linkageReference = (form, configs) => {
   form.setFieldsValue({
     showRetrievalInfo: !!(
-      configs?.Skill?.tools?.includes('bing') || configs?.ConfigKnowledge?.knowledgebase
+      configs?.Skill?.tools?.includes('Bing Search API') || configs?.ConfigKnowledge?.knowledgebase
     ),
   });
 };
